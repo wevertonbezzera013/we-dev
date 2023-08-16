d777678faf2d623843839d67e04c94324f0f3fef
Weverton <wevertonbezzera013@gmail.com>
2023-08-16 17:55:44 -0300

    tweak: vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from "react";
 import { HeroSection } from "./components/hero-section/HeroSection";
 import { ProjectsSection } from "./components/projects-section/ProjectsSection";
 import { Skills } from "./components/skills/Skills";
@@ -5,13 +8,18 @@ import { TestimonialsSection } from "./components/testimonials-section/Testimoni
 import { WorkExperienceSection } from "./components/work-experience-section/WorkExperienceSection";
 
 import { HomePageData } from "./types/page-info";
-import { fetchHygraphQuery } from "./lib/fetch-hygraph-query";
+import { GraphQLClient } from "graphql-request";
 
 export const metadata = {
     title: "Home",
 };
 
-const getPageData = async (): Promise<HomePageData> => {
+// Define the prop type for the Home component
+type InitialPageProps = {
+    pageData: HomePageData | null;
+};
+
+const getPageData = async (): Promise<HomePageData | null> => {
     const query = `
     query MyQuery {
         page(where: {slug: "home"}) {
@@ -38,21 +46,53 @@ const getPageData = async (): Promise<HomePageData> => {
       }
     `;
 
-    return fetchHygraphQuery(query);
+    const hygraphUrl = process.env.HYGRAPH_URL;
+    const hygraphToken = process.env.HYGRAPH_TOKEN;
+
+    if (!hygraphUrl || !hygraphToken) {
+        console.error(
+            "HYGRAPH_URL and/or HYGRAPH_TOKEN are not defined in the environment."
+        );
+        return null;
+    }
+
+    const client = new GraphQLClient(hygraphUrl, {
+        headers: {
+            Authorization: `Bearer ${hygraphToken}`,
+        },
+    });
+
+    try {
+        const data: { page: HomePageData } = await client.request(query);
+        return data.page;
+    } catch (error) {
+        console.error("Error fetching page data:", error);
+        return null;
+    }
 };
 
-export default async function Home() {
-    const { page: pageData } = await getPageData();
+// Use the typed prop in the Home component
+export default function Home({ pageData: initialPageData }: InitialPageProps) {
+    const [pageData, setPageData] = React.useState(initialPageData);
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const data = await getPageData();
+            setPageData(data);
+        }
+        if (!pageData) {
+            fetchData();
+        }
+    }, [pageData]);
 
     if (!pageData) {
-        // Return a fallback UI or loading state if homeInfo is undefined
         return <div className="container">Loading...</div>;
     }
 
     return (
         <>
-            <HeroSection homeInfo={pageData || null} />
-            <Skills tech={pageData?.technologies || null} />
+            <HeroSection homeInfo={pageData?.page || null} />
+            <Skills tech={pageData?.page?.technologies || null} />
             <ProjectsSection />
             <TestimonialsSection />
             <WorkExperienceSection />
